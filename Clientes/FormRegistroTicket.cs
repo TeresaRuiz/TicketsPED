3e767c5e6733359e17189420a4b96f1d7f0c1d13
Marco Rivas <marco.rivas.ochoa@gmail.com>
2026-05-18 16:17:17 -0600

    Arreglo en carga de estados
    Estados no eran cargados en el formulario de registro de tickets para clientes.

--- Clientes/FormRegistroTicket.cs
+++ Clientes/FormRegistroTicket.cs
@@ -111,7 +111,7 @@ namespace TicketsMDB
                 cn = db.AbrirConexion();
 
                 SqlDataAdapter da = new SqlDataAdapter(
-                    "SELECT IdEstado, NombreEstado FROM Estados WHERE IdEstado = 4",
+                    "SELECT IdEstado, NombreEstado FROM Estados",
                     cn);
 
                 DataTable dt = new DataTable();
